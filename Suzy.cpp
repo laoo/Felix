8d07ce1f23d18d0b92ce9742e45bd22fc64d90ea
Waldemar Pawlaszek <waldemar.pawlaszek@gmail.com>
2020-05-14 17:48:22 +0200

    suzy progress

--- Suzy.cpp
+++ Suzy.cpp
@@ -1,7 +1,7 @@
 #include "Suzy.hpp"
 #include <cassert>
 
-Suzy::Suzy()
+Suzy::Suzy() : mEngine{}
 {
 }
 
@@ -16,12 +16,148 @@ uint8_t Suzy::read( uint16_t address )
 
   switch ( address )
   {
+  case TMPADR:
+    break;
+  case TMPADR + 1:
+    break;
+  case TILTACUM:
+    break;
+  case TILTACUM + 1:
+    break;
+  case HOFF:
+    break;
+  case HOFF + 1:
+    break;
+  case VOFF:
+    break;
+  case VOFF + 1:
+    break;
+  case VIDBAS:
+    break;
+  case VIDBAS + 1:
+    break;
+  case COLLBAS:
+    break;
+  case COLLBAS + 1:
+    break;
+  case VIDADR:
+    break;
+  case VIDADR + 1:
+    break;
+  case COLLADR:
+    break;
+  case COLLADR + 1:
+    break;
+  case SCBNEXT:
+    break;
+  case SCBNEXT + 1:
+    break;
+  case SPRDLINE:
+    break;
+  case SPRDLINE + 1:
+    break;
+  case HPOSSTRT:
+    break;
+  case HPOSSTRT + 1:
+    break;
+  case VPOSSTRT:
+    break;
+  case VPOSSTRT + 1:
+    break;
+  case SPRHSIZ:
+    break;
+  case SPRHSIZ + 1:
+    break;
+  case SPRVSIZ:
+    break;
+  case SPRVSIZ + 1:
+    break;
+  case STRETCH:
+    break;
+  case STRETCH + 1:
+    break;
+  case TILT:
+    break;
+  case TILT + 1:
+    break;
+  case SPRDOFF:
+    break;
+  case SPRDOFF + 1:
+    break;
+  case SCVPOS:
+    break;
+  case SCVPOS + 1:
+    break;
+  case COLLOFF:
+    break;
+  case COLLOFF + 1:
+    break;
+  case VSIZACUM:
+    break;
+  case VSIZACUM + 1:
+    break;
+  case HSIZOFF:
+    break;
+  case HSIZOFF + 1:
+    break;
+  case VSIZOFF:
+    break;
+  case VSIZOFF + 1:
+    break;
+  case SCBADR:
+    break;
+  case SCBADR + 1:
+    break;
+  case PROCADR:
+    break;
+  case PROCADR + 1:
+    break;
+
+  case MATHD:
+    break;
+  case MATHC:
+    break;
+  case MATHB:
+    break;
+  case MATHA:
+    break;
+  case MATHP:
+    break;
+  case MATHN:
+
+  case MATHH:
+    break;
+  case MATHG:
+    break;
+  case MATHF:
+    break;
+  case MATHE:
+    break;
+
+  case MATHM:
+    break;
+  case MATHL:
+    break;
+  case MATHK:
+    break;
+  case MATHJ:
+    break;
+
+  case SPRINIT:
+    break;
   case SUZYHREV:
     return 0x01;
+  case SUZYBUSEN:
+    break;
+  case SPRSYS:
+    break;
+
   default:
     assert( false );
+    break;
   }
-  return uint8_t();
+
+  return uint8_t( 0xff );
 }
 
 SequencedAction Suzy::write( uint16_t address, uint8_t value )
@@ -30,16 +166,207 @@ SequencedAction Suzy::write( uint16_t address, uint8_t value )
 
   switch ( address )
   {
-  case HOFF:
-  case VOFF:
-  case COLLOFF:
-  case HSIZOFF:
-  case VSIZOFF:
-  case SPRINIT:
-  case SUZYBUSEN:
-  case SPRSYS:
-    break;
-  default:
+    case TMPADR:
+      mEngine.tmpadr = value;
+      break;
+    case TMPADR + 1:
+      mEngine.tmpadr.h = value;
+      break;
+    case TILTACUM:
+      mEngine.tiltacum = value;
+      break;
+    case TILTACUM + 1:
+      break;
+      mEngine.tiltacum.h = value;
+    case HOFF:
+      mEngine.hoff = value;
+      break;
+    case HOFF + 1:
+      mEngine.hoff.h = value;
+      break;
+    case VOFF:
+      mEngine.voff = value;
+      break;
+    case VOFF + 1:
+      mEngine.voff.h = value;
+      break;
+    case VIDBAS:
+      mEngine.vidbas = value;
+      break;
+    case VIDBAS + 1:
+      mEngine.vidbas.h = value;
+      break;
+    case COLLBAS:
+      mEngine.collbas = value;
+      break;
+    case COLLBAS + 1:
+      mEngine.collbas.h = value;
+      break;
+    case VIDADR:
+      mEngine.vidadr = value;
+      break;
+    case VIDADR + 1:
+      mEngine.vidadr.h = value;
+      break;
+    case COLLADR:
+      mEngine.colladr = value;
+      break;
+    case COLLADR + 1:
+      mEngine.colladr.h = value;
+      break;
+    case SCBNEXT:
+      mEngine.scbnext = value;
+      break;
+    case SCBNEXT + 1:
+      mEngine.scbnext.h = value;
+      break;
+    case SPRDLINE:
+      mEngine.sprdline = value;
+      break;
+    case SPRDLINE + 1:
+      mEngine.sprdline.h = value;
+      break;
+    case HPOSSTRT:
+      mEngine.hposstrt = value;
+      break;
+    case HPOSSTRT + 1:
+      mEngine.hposstrt.h = value;
+      break;
+    case VPOSSTRT:
+      mEngine.vposstrt = value;
+      break;
+    case VPOSSTRT + 1:
+      mEngine.vposstrt.h = value;
+      break;
+    case SPRHSIZ:
+      mEngine.sprhsiz = value;
+      break;
+    case SPRHSIZ + 1:
+      mEngine.sprhsiz.h = value;
+      break;
+    case SPRVSIZ:
+      mEngine.sprvsiz = value;
+      break;
+    case SPRVSIZ + 1:
+      mEngine.sprvsiz.h = value;
+      break;
+    case STRETCH:
+      mEngine.stretch = value;
+      break;
+    case STRETCH + 1:
+      mEngine.stretch.h = value;
+      break;
+    case TILT:
+      mEngine.tilt = value;
+      break;
+    case TILT + 1:
+      mEngine.tilt.h = value;
+      break;
+    case SPRDOFF:
+      mEngine.sprdoff = value;
+      break;
+    case SPRDOFF + 1:
+      mEngine.sprdoff.h = value;
+      break;
+    case SCVPOS:
+      mEngine.scvpos = value;
+      break;
+    case SCVPOS + 1:
+      mEngine.scvpos.h = value;
+      break;
+    case COLLOFF:
+      mEngine.colloff = value;
+      break;
+    case COLLOFF + 1:
+      mEngine.colloff.h = value;
+      break;
+    case VSIZACUM:
+      mEngine.vsizacum = value;
+      break;
+    case VSIZACUM + 1:
+      mEngine.vsizacum.h = value;
+      break;
+    case HSIZOFF:
+      mEngine.hsizoff = value;
+      break;
+    case HSIZOFF + 1:
+      mEngine.hsizoff.h = value;
+      break;
+    case VSIZOFF:
+      mEngine.vsizoff = value;
+      break;
+    case VSIZOFF + 1:
+      mEngine.vsizoff.h = value;
+      break;
+    case SCBADR:
+      mEngine.scbadr = value;
+      break;
+    case SCBADR + 1:
+      mEngine.scbadr.h = value;
+      break;
+    case PROCADR:
+      mEngine.procadr = value;
+      break;
+    case PROCADR + 1:
+      mEngine.procadr.h = value;
+      break;
+
+    case MATHD:
+      mEngine.mathd = value;
+      break;
+    case MATHC:
+      mEngine.mathc = value;
+      break;
+    case MATHB:
+      mEngine.mathb = value;
+      break;
+    case MATHA:
+      mEngine.matha = value;
+      break;
+    case MATHP:
+      mEngine.mathp = value;
+      break;
+    case MATHN:
+      mEngine.mathn = value;
+      break;
+
+    case MATHH:
+      mEngine.mathh = value;
+      break;
+    case MATHG:
+      mEngine.mathg = value;
+      break;
+    case MATHF:
+      mEngine.mathf = value;
+      break;
+    case MATHE:
+      mEngine.mathe = value;
+      break;
+
+    case MATHM:
+      mEngine.mathm = value;
+      break;
+    case MATHL:
+      mEngine.mathl = value;
+      break;
+    case MATHK:
+      mEngine.mathk = value;
+      break;
+    case MATHJ:
+      mEngine.mathj = value;
+      break;
+
+    case SPRINIT:
+      //break;
+    case SUZYHREV:
+      //break;
+    case SUZYBUSEN:
+      mBusEnable = ( SUZYBUSEN::ENABLE & value ) != 0;
+      break;
+    case SPRSYS:
+      //break;
+
+    default:
     assert( false );
     break;
   }
--- Suzy.hpp
+++ Suzy.hpp
@@ -12,15 +12,232 @@ public:
   SequencedAction write( uint16_t address, uint8_t value );
 
 
-  static constexpr uint16_t HOFF        = 0x04;
-  static constexpr uint16_t VOFF        = 0x06;
-  static constexpr uint16_t COLLOFF     = 0x24;
-  static constexpr uint16_t HSIZOFF     = 0x28;
-  static constexpr uint16_t VSIZOFF     = 0x2a;
-  static constexpr uint16_t SPRINIT     = 0x83;
-  static constexpr uint16_t SUZYHREV    = 0x88;
-  static constexpr uint16_t SUZYBUSEN   = 0x90;
-  static constexpr uint16_t SPRSYS      = 0x92;
+  static constexpr uint16_t TMPADR    = 0x00;
+  static constexpr uint16_t TILTACUM  = 0x02;
+  static constexpr uint16_t HOFF      = 0x04;
+  static constexpr uint16_t VOFF      = 0x06;
+  static constexpr uint16_t VIDBAS    = 0x08;
+  static constexpr uint16_t COLLBAS   = 0x0a;
+  static constexpr uint16_t VIDADR    = 0x0c;
+  static constexpr uint16_t COLLADR   = 0x0e;
+  static constexpr uint16_t SCBNEXT   = 0x10;
+  static constexpr uint16_t SPRDLINE  = 0x12;
+  static constexpr uint16_t HPOSSTRT  = 0x14;
+  static constexpr uint16_t VPOSSTRT  = 0x16;
+  static constexpr uint16_t SPRHSIZ   = 0x18;
+  static constexpr uint16_t SPRVSIZ   = 0x1a;
+  static constexpr uint16_t STRETCH   = 0x1c;
+  static constexpr uint16_t TILT      = 0x1e;
+  static constexpr uint16_t SPRDOFF   = 0x20;
+  static constexpr uint16_t SCVPOS    = 0x22;
+  static constexpr uint16_t COLLOFF   = 0x24;
+  static constexpr uint16_t VSIZACUM  = 0x26;
+  static constexpr uint16_t HSIZOFF   = 0x28;
+  static constexpr uint16_t VSIZOFF   = 0x2a;
+  static constexpr uint16_t SCBADR    = 0x2c;
+  static constexpr uint16_t PROCADR   = 0x2e;
 
+  static constexpr uint16_t MATHD     = 0x52;
+  static constexpr uint16_t MATHC     = 0x53;
+  static constexpr uint16_t MATHB     = 0x54;
+  static constexpr uint16_t MATHA     = 0x55;
+  static constexpr uint16_t MATHP     = 0x56;
+  static constexpr uint16_t MATHN     = 0x57;
+
+  static constexpr uint16_t MATHH     = 0x60;
+  static constexpr uint16_t MATHG     = 0x61;
+  static constexpr uint16_t MATHF     = 0x62;
+  static constexpr uint16_t MATHE     = 0x63;
+
+  static constexpr uint16_t MATHM     = 0x6c;
+  static constexpr uint16_t MATHL     = 0x6d;
+  static constexpr uint16_t MATHK     = 0x6e;
+  static constexpr uint16_t MATHJ     = 0x6f;
+
+  static constexpr uint16_t SPRCTL0   = 0x80;
+  static constexpr uint16_t SPRCTL1   = 0x81;
+  static constexpr uint16_t SPRCOLL   = 0x82;
+  static constexpr uint16_t SPRINIT   = 0x83;
+  static constexpr uint16_t SUZYHREV  = 0x88;
+  static constexpr uint16_t SUZYSREV  = 0x89;
+  static constexpr uint16_t SUZYBUSEN = 0x90;
+  static constexpr uint16_t SPRGO     = 0x91;
+  static constexpr uint16_t SPRSYS    = 0x92;
+  static constexpr uint16_t JOYSTICK  = 0xb0;
+  static constexpr uint16_t SWITCHES  = 0xb1;
+  static constexpr uint16_t RCART0    = 0xb2;
+  static constexpr uint16_t RCART1    = 0xb3;
+
+
+
+private:
+
+  struct SPRCTL0
+  {
+    static constexpr uint8_t BITS_MASK         = 0b11000000;
+    static constexpr uint8_t ONE_PER_PIXEL     = 0b00000000;
+    static constexpr uint8_t TWO_PER_PIXEL     = 0b01000000;
+    static constexpr uint8_t THREE_PER_PIXEL   = 0b10000000;
+    static constexpr uint8_t FOUR_PER_PIXEL    = 0b11000000;
+    static constexpr uint8_t HFLIP             = 0b00100000;
+    static constexpr uint8_t VFLIP             = 0b00010000;
+    static constexpr uint8_t SHADOW_SPRITE     = 0b00000111;
+    static constexpr uint8_t XOR_SPRITE        = 0b00000110;
+    static constexpr uint8_t XOR_SHADOW_SPRITE = 0b00000110;
+    static constexpr uint8_t NONCOLL_SPRITE    = 0b00000101;
+    static constexpr uint8_t NORMAL_SPRITE     = 0b00000100;
+    static constexpr uint8_t BOUNDARY_SPRITE   = 0b00000011;
+    static constexpr uint8_t BSHADOW_SPRITE    = 0b00000010;
+    static constexpr uint8_t BACKNONCOLL_SPRITE= 0b00000001;
+    static constexpr uint8_t BACKGROUND_SPRITE = 0b00000000;
+    static constexpr uint8_t BACK_SHADOW_SPRITE= 0b00000000;
+  };
+
+  struct SPRCTL1
+  {
+    static constexpr uint8_t LITERAL           = 0b10000000;
+    static constexpr uint8_t ALGO_3            = 0b01000000; //broken, do not set this bit!
+    static constexpr uint8_t RELOAD_MASK       = 0b00110000;
+    static constexpr uint8_t RELOAD_NONE       = 0b00000000; //Reload nothing
+    static constexpr uint8_t RELOAD_HV         = 0b00010000; //Reload hsize, vsize
+    static constexpr uint8_t RELOAD_HVS        = 0b00100000; //Reload hsize, vsize, stretch
+    static constexpr uint8_t RELOAD_HVST       = 0b00110000; //Reload hsize, vsize, stretch, tilt
+    static constexpr uint8_t REUSE_PALETTE     = 0b00001000;
+    static constexpr uint8_t SKIP_SPRITE       = 0b00000100;
+    static constexpr uint8_t DRAW_UP           = 0b00000010;
+    static constexpr uint8_t DRAW_LEFT         = 0b00000001;
+  };
+
+  struct SPRCOLL
+  {
+    static constexpr uint8_t NO_COLLIDE        = 0b00100000;
+  };
+
+
+  struct SUZYBUSEN
+  {
+    static constexpr uint8_t ENABLE   = 0b00000001; //Suzy Bus Enable, 0 = disabled
+  };
+
+  struct SPRSYS
+  {
+    //These are the SPRSYS flag definitions when writing
+    static constexpr uint8_t SIGNMATH          = 0b10000000; //Signmath: 0 = unsigned math, 1 = signed math.
+    static constexpr uint8_t ACCUMULATE        = 0b01000000; //OK to accumvlate : 0 = do not accumulate, 1 = yes, accumulate.
+    static constexpr uint8_t NO_COLLIDE        = 0b00100000; //dont collide : 1 = dont collide with any sprites.
+    static constexpr uint8_t VSTRETCH          = 0b00010000; //Vstretch: 1 = stretch the v, 0 = Don't play with it, it will grow by itself.
+    static constexpr uint8_t LEFTHAND          = 0b00001000; //Lefthand: 0 = normal handed
+    static constexpr uint8_t UNSAFEACCESSRST   = 0b00000100; //Clear the 'unsafeAccess' bit: 1 = clear it, 0 = no change.
+    static constexpr uint8_t SPRITESTOP        = 0b00000010; //Stop at end of current sprite : 1 = request to stop.Continue sprite processing by setting the Sprite Process Start Bit.Either setting or clearing the SPSB will clear this stop request.
+    //These are the SPRSYS flag definitions when reading
+    static constexpr uint8_t MATHWORKING       = 0b10000000; //Math in process
+    static constexpr uint8_t MATHWARNING       = 0b01000000; //Mathbit: If mult, 1 = accumulator overflow.If div, 1 = div by zero attempted.
+    static constexpr uint8_t MATHCARRY         = 0b00100000; //Last carry bit.
+    static constexpr uint8_t VSTRETCHING       = 0b00010000; //Vstretch
+    static constexpr uint8_t LEFTHANDED        = 0b00001000; //Lefthand
+    static constexpr uint8_t UNSAFEACCESS      = 0b00000100; //UnsafeAccess: 1 = Unsafe Access was performed.
+    static constexpr uint8_t SPRITETOSTOP      = 0b00000010; //Stop at end of current sprite : 1 = request to stop.
+    static constexpr uint8_t SPRITEWORKING     = 0b00000001; //Sprite process was started and has neither completed nor been stopped.
+  };
+
+  struct JOYSTICK
+  {
+    static constexpr uint8_t DOWN              = 0b10000000;
+    static constexpr uint8_t UP                = 0b01000000;
+    static constexpr uint8_t RIGHT             = 0b00100000;
+    static constexpr uint8_t LEFT              = 0b00010000;
+    static constexpr uint8_t OPTION1           = 0b00001000;
+    static constexpr uint8_t OPTION2           = 0b00000100;
+    static constexpr uint8_t INNER             = 0b00000010;
+    static constexpr uint8_t OUTER             = 0b00000001;
+    static constexpr uint8_t A                 = OUTER;
+    static constexpr uint8_t B                 = INNER;
+    static constexpr uint8_t RESTART           = OPTION1;
+    static constexpr uint8_t FLIP              = OPTION2;
+  };
+
+  struct SWITCHES
+  {
+    static constexpr uint8_t CART1_IO_INACTIVE = 0b00000100;
+    static constexpr uint8_t CART0_IO_INACTIVE = 0b00000010;
+    static constexpr uint8_t PAUSE_SWITCH      = 0b00000001;
+  };
+
+  struct Reg
+  {
+    union
+    {
+      uint16_t w;
+      struct
+      {
+        uint8_t l;
+        uint8_t h;
+      };
+    };
+
+    Reg() : w{} {}
+
+    Reg & operator=( uint16_t value ) { w = value; return *this; }
+    operator uint16_t() const { return w; }
+  };
+
+private:
+  struct Engine
+  {
+    Reg tmpadr;
+    Reg tiltacum;
+    Reg hoff;
+    Reg voff;
+    Reg vidbas;
+    Reg collbas;
+    Reg vidadr;
+    Reg colladr;
+    Reg scbnext;
+    Reg sprdline;
+    Reg hposstrt;
+    Reg vposstrt;
+    Reg sprhsiz;
+    Reg sprvsiz;
+    Reg stretch;
+    Reg tilt;
+    Reg sprdoff;
+    Reg scvpos;
+    Reg colloff;
+    Reg vsizacum;
+    Reg hsizoff;
+    Reg vsizoff;
+    Reg scbadr;
+    Reg procadr;
+
+    uint8_t mathd;
+    uint8_t mathc;
+    uint8_t mathb;
+    uint8_t matha;
+    uint8_t mathp;
+    uint8_t mathn;
+
+    uint8_t mathh;
+    uint8_t mathg;
+    uint8_t mathf;
+    uint8_t mathe;
+
+    uint8_t mathm;
+    uint8_t mathl;
+    uint8_t mathk;
+    uint8_t mathj;
+  } mEngine;
+  bool mBusEnable;          //Suzy Bus Enable, 0 = disabled
+  bool mSignMath;           //Signmath: 0 = unsigned math, 1 = signed math.
+  bool mAccumulate;         //OK to accumvlate : 0 = do not accumulate, 1 = yes, accumulate.
+  bool mNoCollide;          //dont collide : 1 = dont collide with any sprites.
+  bool mVStretch;           //Vstretch: 1 = stretch the v, 0 = Don't play with it, it will grow by itself.
+  bool mLeftHand;           //Lefthand: 0 = normal handed
+  bool mUnsafeAccess;       //Clear the 'unsafeAccess' bit: 1 = clear it, 0 = no change.
+  bool mSpriteStop;        //Stop at end of current sprite : 1 = request to stop.Continue sprite processing by setting the Sprite Process Start Bit.Either setting or clearing the SPSB will clear this stop request.
+  bool mMathWorking;       //Math in process
+  bool mMathWarning;       //Mathbit: If mult, 1 = accumulator overflow.If div, 1 = div by zero attempted.
+  bool mMathCarry;         //Last carry bit.
+  bool mVStretching;       //Vstretch
+  bool mSpriteWorking;     //Sprite process was started and has neither completed nor been stopped.
 };
 
