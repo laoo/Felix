c4ab7167d0dc0ba8f111da7d382e8019997a5c02
Waldemar Pawlaszek <waldemar.pawlaszek@gmail.com>
2020-05-24 09:01:53 +0200

    state func table stub

--- VidOperator.cpp
+++ VidOperator.cpp
@@ -1,6 +1,26 @@
 #include "VidOperator.hpp"
+#include <utility>
 
-VidOperator::VidOperator()
+template<int V>
+VidOperator::MemOp stateFun( VidOperator::State & s )
+{
+  if constexpr ( V == 42 )
+    s.v = V;
+  else
+    s.v = V * 2;
+
+  return VidOperator::MemOp{};
+}
+
+template<int... I>
+static std::array<VidOperator::StateFuncT, VidOperator::STATEFUN_SIZE> makeStateFunc( std::integer_sequence<int, I...> )
+{
+  return { stateFun<I>... };
+
+}
+
+
+VidOperator::VidOperator() : mStateFuncs{ makeStateFunc( std::make_integer_sequence<int, VidOperator::STATEFUN_SIZE>{} ) }
 {
 }
 
--- VidOperator.hpp
+++ VidOperator.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "Suzy.hpp"
+#include <array>
 
 class VidOperator
 {
@@ -25,9 +26,48 @@ public:
     }
   };
 
+  struct State
+  {
+    int v;
+  };
+
 public:
   VidOperator();
 
   MemOp flush();
 
+  typedef MemOp( *StateFuncT )( VidOperator::State & s );
+
+  static constexpr size_t STATEFUN_SIZE = 1 << 6;
+
+private:
+
+  std::array<StateFuncT, STATEFUN_SIZE> mStateFuncs;
+
 };
+
+/*
+
+typedef int (*funct)();
+
+template<int V>
+constexpr int fun()
+{
+    if constexpr ( V == 3 )
+        return 1;
+    else return V + 3;
+}
+
+template<int... I>
+constexpr std::array<funct, 5> make(std::integer_sequence<int, I...>)
+{
+    return { fun<I>... };
+}
+
+int main( int argc, char** argv )
+{
+    static constexpr std::array<funct, 5> a{ make( std::make_integer_sequence<int, 5>{} ) };
+    return a[argc]();
+}
+
+*/
