032cd73152970505f3f4d7c27c7bcc945b71b3e2
Waldemar Pawlaszek <waldemar.pawlaszek@gmail.com>
2021-05-04 15:13:57 +0200

    Improving suzy coroutine

--- felix/SuzyProcess.cpp
+++ felix/SuzyProcess.cpp
@@ -5,16 +5,13 @@
 #include "Log.hpp"
 #include "SpriteLineParser.hpp"
 
-SuzyProcess::SuzyProcess( Suzy & suzy ) : mSuzy{ suzy }, mScb{ mSuzy.mSCB }, mBaseCoroutine{}, mEveron{}
+SuzyProcess::SuzyProcess( Suzy & suzy ) : mSuzy{ suzy }, mScb{ mSuzy.mSCB }, mProcessCoroutine{ process() }, mEveron{}
 {
-  auto p = process();
-  mBaseCoroutine = std::move( p );
 }
 
 SuzyProcess::Request const * SuzyProcess::advance()
 {
-  assert( !mCoro.done() );
-  mCoro();
+  mProcessCoroutine.resume();
   return &request;
 }
 
@@ -28,46 +25,6 @@ void SuzyProcess::setFinish()
   requestFinish = ISuzyProcess::RequestFinish{};
 }
 
-void SuzyProcess::setRead( uint16_t address )
-{
-  requestRead = ISuzyProcess::RequestRead{ address };
-}
-
-void SuzyProcess::setRead4( uint16_t address )
-{
-  requestRead4 = ISuzyProcess::RequestRead4{ address };
-}
-
-void SuzyProcess::setWrite( uint16_t address, uint8_t value )
-{
-  requestWrite = ISuzyProcess::RequestWrite{ address, value };
-}
-
-void SuzyProcess::setColRMW( uint16_t address, uint32_t mask, uint8_t value )
-{
-  requestWrite4 = ISuzyProcess::RequestColRMW{ address, mask, value };
-}
-
-void SuzyProcess::setVidRMW( uint16_t address, uint8_t value, uint8_t mask )
-{
-  requestVidRMW = ISuzyProcess::RequestVidRMW{ address, value, mask };
-}
-
-void SuzyProcess::setXor( uint16_t address, uint8_t value )
-{
-  requestXOR = ISuzyProcess::RequestXOR{ address, value };
-}
-
-SuzyProcess::Response const& SuzyProcess::getResponse() const
-{
-  return response;
-}
-
-void SuzyProcess::setHandle( std::coroutine_handle<> c )
-{
-  mCoro = c;
-}
-
 SuzyProcess::ProcessCoroutine SuzyProcess::process()
 {
   auto & scb = mScb;
@@ -77,21 +34,21 @@ SuzyProcess::ProcessCoroutine SuzyProcess::process()
     scb.scbadr = scb.scbnext;
     scb.tmpadr = scb.scbadr;
 
-    mSuzy.writeSPRCTL0( co_await SuzyRead{ scb.tmpadr++ } );
-    mSuzy.writeSPRCTL1( co_await SuzyRead{ scb.tmpadr++ } );
-    mSuzy.writeSPRCOLL( co_await SuzyRead{ scb.tmpadr++ } );
-    scb.scbnext.l = co_await SuzyRead{ scb.tmpadr++ };
-    scb.scbnext.h = co_await SuzyRead{ scb.tmpadr++ };
+    mSuzy.writeSPRCTL0( co_await suzyRead( scb.tmpadr++ ) );
+    mSuzy.writeSPRCTL1( co_await suzyRead( scb.tmpadr++ ) );
+    mSuzy.writeSPRCOLL( co_await suzyRead( scb.tmpadr++ ) );
+    scb.scbnext.l = co_await suzyRead( scb.tmpadr++ );
+    scb.scbnext.h = co_await suzyRead( scb.tmpadr++ );
 
     if ( mSuzy.mSkipSprite )
       co_return;
 
-    scb.sprdline.l = co_await SuzyRead{ scb.tmpadr++ };
-    scb.sprdline.h = co_await SuzyRead{ scb.tmpadr++ };
-    scb.hposstrt.l = co_await SuzyRead{ scb.tmpadr++ };
-    scb.hposstrt.h = co_await SuzyRead{ scb.tmpadr++ };
-    scb.vposstrt.l = co_await SuzyRead{ scb.tmpadr++ };
-    scb.vposstrt.h = co_await SuzyRead{ scb.tmpadr++ };
+    scb.sprdline.l = co_await suzyRead( scb.tmpadr++ );
+    scb.sprdline.h = co_await suzyRead( scb.tmpadr++ );
+    scb.hposstrt.l = co_await suzyRead( scb.tmpadr++ );
+    scb.hposstrt.h = co_await suzyRead( scb.tmpadr++ );
+    scb.vposstrt.l = co_await suzyRead( scb.tmpadr++ );
+    scb.vposstrt.h = co_await suzyRead( scb.tmpadr++ );
 
     scb.tilt = 0;
     scb.stretch = 0;
@@ -99,28 +56,28 @@ SuzyProcess::ProcessCoroutine SuzyProcess::process()
     switch ( mSuzy.mReload )
     {
     case Suzy::Reload::HVST:  //Reload hsize, vsize, stretch, tilt
-      scb.sprhsiz.l = co_await SuzyRead{ scb.tmpadr++ };
-      scb.sprhsiz.h = co_await SuzyRead{ scb.tmpadr++ };
-      scb.sprvsiz.l = co_await SuzyRead{ scb.tmpadr++ };
-      scb.sprvsiz.h = co_await SuzyRead{ scb.tmpadr++ };
-      scb.stretch.l = co_await SuzyRead{ scb.tmpadr++ };
-      scb.stretch.h = co_await SuzyRead{ scb.tmpadr++ };
-      scb.tilt.l = co_await SuzyRead{ scb.tmpadr++ };
-      scb.tilt.h = co_await SuzyRead{ scb.tmpadr++ };
+      scb.sprhsiz.l = co_await suzyRead( scb.tmpadr++ );
+      scb.sprhsiz.h = co_await suzyRead( scb.tmpadr++ );
+      scb.sprvsiz.l = co_await suzyRead( scb.tmpadr++ );
+      scb.sprvsiz.h = co_await suzyRead( scb.tmpadr++ );
+      scb.stretch.l = co_await suzyRead( scb.tmpadr++ );
+      scb.stretch.h = co_await suzyRead( scb.tmpadr++ );
+      scb.tilt.l = co_await suzyRead( scb.tmpadr++ );
+      scb.tilt.h = co_await suzyRead( scb.tmpadr++ );
       break;
     case Suzy::Reload::HVS:   //Reload hsize, vsize, stretch
-      scb.sprhsiz.l = co_await SuzyRead{ scb.tmpadr++ };
-      scb.sprhsiz.h = co_await SuzyRead{ scb.tmpadr++ };
-      scb.sprvsiz.l = co_await SuzyRead{ scb.tmpadr++ };
-      scb.sprvsiz.h = co_await SuzyRead{ scb.tmpadr++ };
-      scb.stretch.l = co_await SuzyRead{ scb.tmpadr++ };
-      scb.stretch.h = co_await SuzyRead{ scb.tmpadr++ };
+      scb.sprhsiz.l = co_await suzyRead( scb.tmpadr++ );
+      scb.sprhsiz.h = co_await suzyRead( scb.tmpadr++ );
+      scb.sprvsiz.l = co_await suzyRead( scb.tmpadr++ );
+      scb.sprvsiz.h = co_await suzyRead( scb.tmpadr++ );
+      scb.stretch.l = co_await suzyRead( scb.tmpadr++ );
+      scb.stretch.h = co_await suzyRead( scb.tmpadr++ );
       break;
     case Suzy::Reload::HV:    //Reload hsize, vsize
-      scb.sprhsiz.l = co_await SuzyRead{ scb.tmpadr++ };
-      scb.sprhsiz.h = co_await SuzyRead{ scb.tmpadr++ };
-      scb.sprvsiz.l = co_await SuzyRead{ scb.tmpadr++ };
-      scb.sprvsiz.h = co_await SuzyRead{ scb.tmpadr++ };
+      scb.sprhsiz.l = co_await suzyRead( scb.tmpadr++ );
+      scb.sprhsiz.h = co_await suzyRead( scb.tmpadr++ );
+      scb.sprvsiz.l = co_await suzyRead( scb.tmpadr++ );
+      scb.sprvsiz.h = co_await suzyRead( scb.tmpadr++ );
       break;
     case Suzy::Reload::NONE:  //Reload nothing
       break;
@@ -138,9 +95,9 @@ SuzyProcess::ProcessCoroutine SuzyProcess::process()
         };
       };
 
-      p0 = co_await SuzyRead4{ scb.tmpadr };
+      p0 = co_await suzyRead4( scb.tmpadr );
       scb.tmpadr += 4;
-      p1 = co_await SuzyRead4{ scb.tmpadr };
+      p1 = co_await suzyRead4( scb.tmpadr );
       scb.tmpadr += 4;
 
       //TODO: implement bug:
@@ -153,33 +110,33 @@ SuzyProcess::ProcessCoroutine SuzyProcess::process()
       }
     }
 
-    mSuzy.mDisableCollisions = mSuzy.mNoCollide |
-      ( ( mSuzy.mSprColl & Suzy::SPRCOLL::NO_COLLIDE ) == 1 ) |
-      ( mSuzy.mSpriteType == Suzy::Sprite::BACKNONCOLL ) |
+    mSuzy.mDisableCollisions = mSuzy.mNoCollide ||
+      ( ( mSuzy.mSprColl & Suzy::SPRCOLL::NO_COLLIDE ) == 1 ) ||
+      ( mSuzy.mSpriteType == Suzy::Sprite::BACKNONCOLL ) ||
       ( mSuzy.mSpriteType == Suzy::Sprite::NONCOLL );
 
     mSuzy.mFred = std::nullopt;
 
     {
-      auto & self = *this;
+      auto & suzy = mSuzy;
 
-      VidOperator vidOp{ self.mSuzy.mSpriteType };
-      ColOperator colOp{ self.mSuzy.mSpriteType, (uint8_t)(self.mSuzy.mSprColl & Suzy::SPRCOLL::NUMBER_MASK) };
+      VidOperator vidOp{ suzy.mSpriteType };
+      ColOperator colOp{ suzy.mSpriteType, (uint8_t)(suzy.mSprColl & Suzy::SPRCOLL::NUMBER_MASK) };
 
-      self.mEveron = false;
+      mEveron = false;
 
-      auto const& quadCycle = self.mSuzy.mQuadrantOrder[(size_t)self.mSuzy.mStartingQuadrant];
+      auto const& quadCycle = suzy.mQuadrantOrder[(size_t)suzy.mStartingQuadrant];
 
       for ( int quadrant = 0; quadrant < 4; ++quadrant )
       {
         int left = ((uint8_t)quadCycle[quadrant] & Suzy::SPRCTL1::DRAW_LEFT) == 0 ? 0 : 1;
         int up = ((uint8_t)quadCycle[quadrant] & Suzy::SPRCTL1::DRAW_UP) == 0 ? 0 : 1;
-        left ^= self.mSuzy.mHFlip ? 1 : 0;
-        up ^= self.mSuzy.mVFlip ? 1 : 0;
+        left ^= suzy.mHFlip ? 1 : 0;
+        up ^= suzy.mVFlip ? 1 : 0;
         scb.tiltacum = 0;
         scb.vsizacum = (up == 0) ? scb.vsizoff.w : 0;
         scb.sprvpos = scb.vposstrt - scb.voff;
-        if ( ((uint8_t)quadCycle[quadrant] & Suzy::SPRCTL1::DRAW_UP) != ((uint8_t)quadCycle[(size_t)self.mSuzy.mStartingQuadrant] & Suzy::SPRCTL1::DRAW_UP) )
+        if ( ((uint8_t)quadCycle[quadrant] & Suzy::SPRCTL1::DRAW_UP) != ((uint8_t)quadCycle[(size_t)suzy.mStartingQuadrant] & Suzy::SPRCTL1::DRAW_UP) )
           scb.sprvpos += up ? -1 : 1;
 
         for ( ;; )
@@ -191,10 +148,10 @@ SuzyProcess::ProcessCoroutine SuzyProcess::process()
           {
             scb.procadr = scb.sprdline;
             Shifter shifter{};
-            shifter.push( co_await SuzyRead4{ scb.procadr } );
+            shifter.push( co_await suzyRead4( scb.procadr ) );
             scb.procadr += 4;
             scb.sprdoff = shifter.pull<8>();
-            SpriteLineParser slp{ shifter, self.mSuzy.mLiteral, self.mSuzy.bpp(), (scb.sprdoff - 1) * 8 };
+            SpriteLineParser slp{ shifter, suzy.mLiteral, suzy.bpp(), (scb.sprdoff - 1) * 8 };
             if ( up == 0 && scb.sprvpos >= Suzy::mScreenHeight || up == 1 && (int16_t)(scb.sprvpos) < 0 ) continue;
             scb.vidadr = scb.vidbas + scb.sprvpos * Suzy::mScreenWidth / 2;
             scb.colladr = scb.collbas + scb.sprvpos * Suzy::mScreenWidth / 2;
@@ -204,14 +161,14 @@ SuzyProcess::ProcessCoroutine SuzyProcess::process()
             scb.tiltacum.h = 0;
             int hsizacum = left == 0 ? scb.hsizoff.w : 0;
             int sprhpos = scb.hposstrt - scb.hoff;
-            if ( ((uint8_t)quadCycle[quadrant] & Suzy::SPRCTL1::DRAW_LEFT) != ((uint8_t)quadCycle[(size_t)self.mSuzy.mStartingQuadrant] & Suzy::SPRCTL1::DRAW_LEFT) )
+            if ( ((uint8_t)quadCycle[quadrant] & Suzy::SPRCTL1::DRAW_LEFT) != ((uint8_t)quadCycle[(size_t)suzy.mStartingQuadrant] & Suzy::SPRCTL1::DRAW_LEFT) )
               sprhpos += left ? -1 : 1;
 
             while ( int const* pen = slp.getPen() )
             {
               if ( shifter.size() < 24 && slp.totalBits() > shifter.size() )
               {
-                shifter.push( co_await SuzyRead{ scb.procadr } );
+                shifter.push( co_await suzyRead( scb.procadr ) );
                 scb.procadr += 1;
               }
 
@@ -224,33 +181,33 @@ SuzyProcess::ProcessCoroutine SuzyProcess::process()
                 // Stop horizontal loop if outside of screen bounds
                 if ( sprhpos < Suzy::mScreenWidth )
                 {
-                  uint8_t pixel = self.mSuzy.mPalette[*pen];
+                  uint8_t pixel = suzy.mPalette[*pen];
 
-                  if ( !self.mSuzy.mDisableCollisions )
+                  if ( !suzy.mDisableCollisions )
                   {
                     if ( auto memOp = colOp.process( sprhpos, pixel ) )
                     {
-                      colOp.receiveHiColl( co_await SuzyColRMW{ memOp.mask, memOp.addr, memOp.value } );
+                      colOp.receiveHiColl( co_await suzyColRMW( memOp.mask, memOp.addr, memOp.value ) );
                     }
                   }
 
                   switch ( auto memOp = vidOp.process( sprhpos, pixel ) )
                   {
                   case VidOperator::MemOp::WRITE:
-                    co_await SuzyWrite{ memOp.addr, memOp.value };
+                    co_await suzyWrite( memOp.addr, memOp.value );
                     break;
                   case VidOperator::MemOp::MODIFY:
                   case VidOperator::MemOp::WRITE | VidOperator::MemOp::MODIFY:
-                    co_await SuzyVidRMW{ memOp.addr, memOp.value, memOp.mask() };
+                    co_await suzyVidRMW( memOp.addr, memOp.value, memOp.mask() );
                     break;
                   case VidOperator::MemOp::XOR:
-                    co_await SuzyXOR{ memOp.addr, memOp.value };
+                    co_await suzyXOR( memOp.addr, memOp.value );
                     break;
                   default:
                     break;
                   }
 
-                  self.mEveron = true;
+                  mEveron = true;
                 }
                 sprhpos += left ? -1 : 1;
               }
@@ -259,17 +216,17 @@ SuzyProcess::ProcessCoroutine SuzyProcess::process()
             switch ( auto memOp = vidOp.flush() )
             {
             case VidOperator::MemOp::XOR:
-              co_await SuzyXOR{ memOp.addr, memOp.value };
+              co_await suzyXOR( memOp.addr, memOp.value );
               break;
             default:
-              co_await SuzyVidRMW{ memOp.addr, memOp.value, memOp.mask() };
+              co_await suzyVidRMW( memOp.addr, memOp.value, memOp.mask() );
               break;
             }
-            if ( !self.mSuzy.mDisableCollisions )
+            if ( !suzy.mDisableCollisions )
             {
               if ( auto memOp = colOp.flush() )
               {
-                colOp.receiveHiColl( co_await SuzyColRMW{ memOp.mask, memOp.addr, memOp.value } );
+                colOp.receiveHiColl( co_await suzyColRMW( memOp.mask, memOp.addr, memOp.value ) );
               }
             }
           }
@@ -281,8 +238,8 @@ SuzyProcess::ProcessCoroutine SuzyProcess::process()
           break;
       }
       auto fred = colOp.hiColl();
-      if ( !self.mSuzy.mDisableCollisions )
-        self.mSuzy.mFred = fred & 0x0f;
+      if ( !suzy.mDisableCollisions )
+        suzy.mFred = fred & 0x0f;
     }
 
     if ( mSuzy.mEveron && mEveron )
@@ -292,7 +249,7 @@ SuzyProcess::ProcessCoroutine SuzyProcess::process()
 
     if ( mSuzy.mFred )
     {
-      co_await SuzyWrite{ (uint16_t)( scb.scbadr + scb.colloff ), *mSuzy.mFred };
+      co_await suzyWrite( (uint16_t)( scb.scbadr + scb.colloff ), *mSuzy.mFred );
     }
 
     if ( mSuzy.mSpriteStop )
--- felix/SuzyProcess.hpp
+++ felix/SuzyProcess.hpp
@@ -5,9 +5,10 @@
 class SuzyProcess : public ISuzyProcess
 {
 public:
-  struct Response : public Request
+  struct Response
   {
-    uint16_t addr;
+    bool await_ready() { return false; }
+    void await_suspend( std::coroutine_handle<> c ) {}
     uint32_t value;
   };
 
@@ -19,23 +20,68 @@ public:
   void respond( uint32_t value ) override;
 
   void setFinish();
-  void setRead( uint16_t address );
-  void setRead4( uint16_t address );
-  void setWrite( uint16_t address, uint8_t value );
-  void setColRMW( uint16_t address, uint32_t mask, uint8_t value );
-  void setVidRMW( uint16_t address, uint8_t value, uint8_t mask );
-  void setXor( uint16_t address, uint8_t value );
-  Response const& getResponse() const;
-  void setHandle( std::coroutine_handle<> c );
 
 private:
 
-  struct SuzyRead { uint16_t address; };
-  struct SuzyRead4 { uint16_t address; };
-  struct SuzyWrite { uint16_t address; uint8_t value; };
-  struct SuzyColRMW { uint32_t mask; uint16_t address;  uint8_t value; };
-  struct SuzyVidRMW { uint16_t address; uint8_t value; uint8_t mask; };
-  struct SuzyXOR { uint16_t address; uint8_t value; };
+  auto & suzyRead( uint16_t address )
+  {
+    struct SuzyReadResponse : public Response
+    {
+      uint8_t await_resume() { return (uint8_t)value; }
+    };
+    requestRead = ISuzyProcess::RequestRead{ address };
+    return static_cast<SuzyReadResponse &>( response );
+  }
+
+  auto & suzyRead4( uint16_t address )
+  {
+    struct SuzyRead4Response : public Response
+    {
+      uint32_t await_resume() { return value; }
+    };
+    requestRead4 = ISuzyProcess::RequestRead4{ address };
+    return static_cast<SuzyRead4Response &>( response );
+  }
+
+  auto & suzyWrite( uint16_t address, uint8_t value )
+  {
+    struct SuzyWriteResponse : public Response
+    {
+      void await_resume() {}
+    };
+    requestWrite = ISuzyProcess::RequestWrite{ address, value };
+    return static_cast<SuzyWriteResponse &>( response );
+  }
+
+  auto & suzyColRMW( uint32_t mask, uint16_t address, uint8_t value )
+  {
+    struct SuzyColRMWResponse : public Response
+    {
+      uint8_t await_resume() { return (uint8_t)value; }
+    };
+    requestWrite4 = ISuzyProcess::RequestColRMW{ address, mask, value };
+    return static_cast<SuzyColRMWResponse &>( response );
+  }
+
+  auto & suzyVidRMW( uint16_t address, uint8_t value, uint8_t mask )
+  {
+    struct SuzyVidRMWResponse : public Response
+    {
+      void await_resume() {}
+    };
+    requestVidRMW = ISuzyProcess::RequestVidRMW{ address, value, mask };
+    return static_cast<SuzyVidRMWResponse &>( response );
+  }
+
+  auto & suzyXOR( uint16_t address, uint8_t value )
+  {
+    struct SuzyXORResponse : public Response
+    {
+      void await_resume() {}
+    };
+    requestXOR = ISuzyProcess::RequestXOR{ address, value };
+    return static_cast<SuzyXORResponse &>( response );
+  }
 
   struct ProcessCoroutine
   {
@@ -47,12 +93,11 @@ private:
     {
       promise_type( SuzyProcess & suzyProcess ) : mSuzyProcess{ suzyProcess }
       {
-        mSuzyProcess.setHandle( std::coroutine_handle<promise_type>::from_promise( *this ) );
       }
 
       auto get_return_object() { return ProcessCoroutine{ std::coroutine_handle<promise_type>::from_promise( *this ) }; }
 
-      auto initial_suspend() { return std::suspend_never{}; }
+      auto initial_suspend() { return std::suspend_always{}; }
       std::suspend_always final_suspend() noexcept
       {
         mSuzyProcess.setFinish();
@@ -62,79 +107,6 @@ private:
       {
       }
       void unhandled_exception() { std::terminate(); }
-
-      auto await_transform( SuzyRead r )
-      {
-        struct Awaiter
-        {
-          SuzyProcess & p;
-          bool await_ready() { return false; }
-          uint8_t await_resume() { return p.getResponse().value; }
-          void await_suspend( std::coroutine_handle<> c ) {}
-        };
-        mSuzyProcess.setRead( r.address );
-        return Awaiter{ mSuzyProcess };
-      }
-      auto await_transform( SuzyRead4 r )
-      {
-        struct Awaiter
-        {
-          SuzyProcess & p;
-          bool await_ready() { return false; }
-          uint32_t await_resume() { return p.getResponse().value; }
-          void await_suspend( std::coroutine_handle<> c ) {}
-        };
-        mSuzyProcess.setRead4( r.address );
-        return Awaiter{ mSuzyProcess };
-      }
-      auto await_transform( SuzyWrite w )
-      {
-        struct Awaiter
-        {
-          SuzyProcess & p;
-          bool await_ready() { return false; }
-          void await_resume() {}
-          void await_suspend( std::coroutine_handle<> c ) {}
-        };
-        mSuzyProcess.setWrite( w.address, w.value );
-        return Awaiter{ mSuzyProcess };
-      }
-      auto await_transform( SuzyColRMW w )
-      {
-        struct Awaiter
-        {
-          SuzyProcess & p;
-          bool await_ready() { return false; }
-          uint8_t await_resume() { return (uint8_t)p.getResponse().value; }
-          void await_suspend( std::coroutine_handle<> c ) {}
-        };
-        mSuzyProcess.setColRMW( w.address, w.mask, w.value );
-        return Awaiter{ mSuzyProcess };
-      }
-      auto await_transform( SuzyVidRMW rmw )
-      {
-        struct Awaiter
-        {
-          SuzyProcess & p;
-          bool await_ready() { return false; }
-          void await_resume() {}
-          void await_suspend( std::coroutine_handle<> c ) {}
-        };
-        mSuzyProcess.setVidRMW( rmw.address, rmw.value, rmw.mask );
-        return Awaiter{ mSuzyProcess };
-      }
-      auto await_transform( SuzyXOR x )
-      {
-        struct Awaiter
-        {
-          SuzyProcess & p;
-          bool await_ready() { return false; }
-          void await_resume() {}
-          void await_suspend( std::coroutine_handle<> c ) {}
-        };
-        mSuzyProcess.setXor( x.address, x.value );
-        return Awaiter{ mSuzyProcess };
-      }
 
     private:
       SuzyProcess & mSuzyProcess;
@@ -193,10 +165,10 @@ private:
     RequestColRMW requestWrite4;
     RequestVidRMW requestVidRMW;
     RequestXOR requestXOR;
-    Response response;
   };
 
-  ProcessCoroutine mBaseCoroutine;
-  std::coroutine_handle<> mCoro;
+  Response response;
+
+  ProcessCoroutine mProcessCoroutine;
   bool mEveron;
 };
