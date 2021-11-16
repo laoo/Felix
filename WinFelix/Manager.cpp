8c41a93df99e58e32ddbb789eb6ae3f9d06bafb1
Waldemar Pawlaszek <waldemar.pawlaszek@gmail.com>
2021-11-16 18:36:23 +0100

    #81 reintroduced simple gui

--- WinFelix/Manager.cpp
+++ WinFelix/Manager.cpp
@@ -97,6 +97,28 @@ void Manager::drawGui( int left, int top, int right, int bottom )
 {
   ImGuiIO & io = ImGui::GetIO();
 
+  bool hovered = io.MousePos.x > left && io.MousePos.y > top && io.MousePos.x < right&& io.MousePos.y < bottom;
+
+  if ( hovered )
+  {
+    ImGui::PushStyleVar( ImGuiStyleVar_Alpha, std::clamp( ( 100.0f - io.MousePos.y ) / 100.f, 0.0f, 1.0f ) );
+    if ( ImGui::BeginMainMenuBar() )
+    {
+      ImGui::PushStyleVar( ImGuiStyleVar_Alpha, 1.0f );
+      if ( ImGui::BeginMenu( "File" ) )
+      {
+        if ( ImGui::MenuItem( "Exit", "Alt+F4" ) )
+        {
+          mEmulationRunning = false;
+        }
+        ImGui::EndMenu();
+      }
+      ImGui::EndMainMenuBar();
+      ImGui::PopStyleVar();
+    }
+    ImGui::PopStyleVar();
+  }
+
   if ( mMonitor && mInstance )
   {
     if ( ImGui::Begin( "Monitor" ) )
