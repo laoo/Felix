fcd8f737d1dc82b890df9e9bc28ddf6e8bcba7ec
Waldemar Pawlaszek <waldemar.pawlaszek@gmail.com>
2022-01-10 15:54:07 +0100

    Refactor: Debugger

---
+++ WinFelix/Debugger.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+#include "Utility.hpp"
+#include "BaseRenderer.hpp"
+
+struct DebugWindow
+{
+  DebugWindow( int id, int columns, int rows ) : id{ id }, columns{ columns }, rows{ rows }
+  {
+    data.resize( columns* rows );
+  }
+
+  int id;
+  int columns;
+  int rows;
+  std::vector<uint8_t> data;
+};
+
+struct ScreenView
+{
+  int id;
+  ScreenViewType type;
+  uint16_t customAddress;
+  bool safePalette;
+};
+
+class Debugger
+{
+public:
+  Debugger();
+  ~Debugger();
+
+  void operator()( RunMode mode );
+
+  bool isPaused() const;
+  bool isDebugMode() const;
+  bool isCPUVisualized() const;
+  bool isDisasmVisualized() const;
+  bool isHistoryVisualized() const;
+  bool isBreakOnBrk() const;
+
+  std::span<ScreenView> screenViews();
+
+  void visualizeCPU( bool value );
+  void visualizeDisasm( bool value );
+  void visualizeHistory( bool value );
+  void debugMode( bool value );
+  bool debugModeOnBreak() const;
+  void debugModeOnBreak( bool value );
+  bool normalModeOnRun() const;
+  void normalModeOnRun( bool value );
+  void breakOnBrk( bool value );
+  void newScreenView();
+  void delScreenView( int id );
+
+  void togglePause();
+
+  DebugWindow& cpuVisualizer();
+  DebugWindow& disasmVisualizer();
+  DebugWindow& historyVisualizer();
+
+  mutable std::mutex mutex;
+
+private:
+  std::vector<ScreenView> mScreenViews;
+  DebugWindow mCpuVisualizer;
+  DebugWindow mDisasmVisualizer;
+  DebugWindow mHistoryVisualizer;
+  bool mDebugMode;
+  bool mVisualizeCPU;
+  bool mVisualizeDisasm;
+  bool mVisualizeHistory;
+  bool mDebugModeOnBreak;
+  bool mNormalModeOnRun;
+  bool mBreakOnBrk;
+  friend class Manager;
+  std::atomic<RunMode> mRunMode;
+
+};
