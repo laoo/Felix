7c83ac06ae86ba5dcf4fe9d9ada3a807c500815f
Waldemar Pawlaszek <waldemar.pawlaszek@gmail.com>
2022-01-27 09:03:01 +0100

    #108 SPRSYS.LEFTHAND is 1 at startup

--- libFelix/Suzy.cpp
+++ libFelix/Suzy.cpp
@@ -6,7 +6,7 @@
 #include "Log.hpp"
 
 Suzy::Suzy( Core & core, std::shared_ptr<IInputSource> inputSource ) : mCore{ core }, mSCB{}, mMath{ mCore.getTraceHelper() }, mInputSource{ inputSource }, mAccessTick{},
-  mPalette{}, mBusEnable{}, mNoCollide{}, mVStretch{}, mLeftHand{}, mUnsafeAccess{}, mSpriteStop{},
+  mPalette{}, mBusEnable{}, mNoCollide{}, mVStretch{}, mLeftHand{ true }, mUnsafeAccess{}, mSpriteStop{},
   mSpriteWorking{}, mHFlip{}, mVFlip{}, mLiteral{}, mAlgo3{}, mReusePalette{}, mSkipSprite{}, mStartingQuadrant{}, mEveron{},
   mBpp{}, mSpriteType{}, mReload{}, mSprColl{}, mSprInit{}
 {
